370a8a1712816e9a037aab18f8df1963689464e2
tiago-sb <belatiago72@gmail.com>
2023-09-27 17:09:26 -0300

    adiciona seção os fetchs ao código main do js

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+// 
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => console.log(jsonBody))
+    .catch((error) => console.log(error))
+
+
+// .then(function(response) {
+//     response.json().then(function(responseBody){
+//         console.log(responseBody)
+// }) 
