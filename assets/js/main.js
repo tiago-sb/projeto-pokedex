61f01617e1b5987775946f3c724abf2775f269aa
tiago-sb <belatiago72@gmail.com>
2023-09-29 07:35:25 -0300

    adiciona a classe Pokemon, que possui o objetivo de receber os atributos dos pokemos e diminuir a verbosidade do código

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,18 @@
-function convertPokemonTypesLi(pokemonTypes) {
-    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
-}
 function convertPokemonToLi(pokemon){
     return `
         <li class="pokemon">
-            <span class="number">#${pokemon.order}</span>
+            <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
             
             <div class="detail">
                 <ol class="types">
-                    ${convertPokemonTypesLi(pokemon.types).join('')}                    
+                    ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}                    
                 </ol>
                 
-                <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
             </div>
         </li>
-    `
+    ` 
 }
 
 const pokemonList = document.getElementById('pokemonList')
@@ -24,4 +21,11 @@ pokeApi.getPokemons().then((pokemons = []) => {
     const newHtml = pokemons.map(convertPokemonToLi).join('')
     pokemonList.innerHTML = newHtml 
 })
+
+
+// function convertPokemonTypesLi(pokemonTypes) {
+//     return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+// }
+// convertPokemonTypesLi(pokemon.types).join('')
+
 
